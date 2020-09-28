1aca041a3d40cc602ddf45391c2dbdd84e1bde87
iikunsama <44876379+iikunsama@users.noreply.github.com>
2020-09-28 15:31:53 +0300

    Update index.php
    arrays

--- 1-5/index.php
+++ 1-5/index.php
@@ -1,1 +1,9 @@
 <?php
+$ar =array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K');
+
+foreach($ar as $value)
+echo $value . " ";
+echo "<br>";
+$b=array_reverse($ar);
+foreach($b as $value)
+echo $value . " ";
